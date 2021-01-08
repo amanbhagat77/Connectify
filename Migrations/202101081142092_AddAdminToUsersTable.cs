f80cb1a0257199283416eaa2896ebf9d1cdf4e81
amanbhagat77 <37319472+amanbhagat77@users.noreply.github.com>
2021-01-08 17:19:16 +0530

    After App Deployed in Azure

---
+++ Migrations/202101081142092_AddAdminToUsersTable.cs
@@ -0,0 +1,17 @@
+﻿namespace Connectify.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAdminToUsersTable : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
